ac8eda4461a4eeb0781add329e83cc44325ebc96
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-02-24 17:47:43 +0530

    Add : Rendering index.html on intial request

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require("./database"); // importing DB connection
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -13,8 +14,11 @@ const studentRoute = require("./routes/student");
 
 app.use(cors());
 app.use(bodyParser.json());
-
 app.use("/", studentRoute);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port :  ${PORT}`);
