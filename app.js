8dde65596379cc113ce94665e70069c6a1e442cb
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-02-24 13:48:32 +0530

    Add : Simple server setup

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+
+const PORT = process.env.PORT || 4000
+
+app.get('/',(req,res)=>{
+  res.send('Server up and running')
+})
+
+app.listen(PORT,()=>{
+  console.log(`Server is up and running on port :  ${PORT}`)
+})
